aef92a7c2c25784a98206e27b9ce570d329419bf
Joshua Soileau <joshua.soileau@gmail.com>
2020-10-11 10:58:33 -0400

    Gets mdx & syntax highlighting working

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/code.css";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      This is the header I think
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
