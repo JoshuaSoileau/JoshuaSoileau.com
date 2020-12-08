5a10a082244c66738946ab029f088961093d74ea
Joshua Soileau <joshua.soileau@gmail.com>
2020-12-08 10:05:31 -0500

    Sets up GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
 import "../styles/code.css";
 import Layout from "../components/Layout";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-557FDGN",
+};
+
+TagManager.initialize(tagManagerArgs);
 
 function MyApp({ Component, pageProps }) {
   return (
