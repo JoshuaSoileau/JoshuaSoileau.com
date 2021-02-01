6a531b250eebfa9de7a664e118322da16e5f9f76
Joshua Soileau <joshua.soileau@gmail.com>
2021-02-01 17:06:08 -0500

    Install all the things

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Header from "@components/Header";
+import "twin.macro";
+
+export default function Layout({ children, pageTitle, ...props }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{pageTitle}</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📖</text></svg>"
+        />
+      </Head>
+      <Header />
+      <section className="layout container text-xl">
+        <div className="content">{children}</div>
+      </section>
+      <footer>Built by me!</footer>
+    </>
+  );
+}
