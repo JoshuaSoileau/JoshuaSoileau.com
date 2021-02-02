46b2e416627aed646905c44d06ea23ef3e49c953
Joshua Soileau <joshua.soileau@gmail.com>
2021-02-02 10:42:28 -0500

    Shift fade in to body content

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -20,14 +20,14 @@ export default function Layout({ children, pageTitle, ...props }) {
           href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📖</text></svg>"
         />
       </Head>
-      <div
-        css={[
-          tw`flex flex-col items-center  min-h-screen`,
-          `opacity: ${opacity}; transition: all 500ms ease;`,
-        ]}
-      >
+      <div tw="flex flex-col items-center  min-h-screen">
         <Header />
-        <main tw="container text-xl flex-grow flex flex-col justify-center py-24">
+        <main
+          css={[
+            tw`container text-xl flex-grow flex flex-col justify-center py-24`,
+            `opacity: ${opacity}; transition: all 500ms ease;`,
+          ]}
+        >
           {children}
         </main>
         <Footer />
