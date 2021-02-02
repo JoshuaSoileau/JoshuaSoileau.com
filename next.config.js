0fdf63cddef04cd6474a3c4b04600c08bc4c4a9c
Joshua Soileau <joshua.soileau@gmail.com>
2021-02-02 10:40:32 -0500

    Defines isServer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   target: "serverless",
-  webpack: function (config) {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
